b11f9a194ac52a43d6a0c0bcfe8e96f8c7333493
yangbin <iyangbin@foxmail.com>
2020-12-14 17:25:09 +0800

    build project

---
+++ demo-hello/src/main/java/com/demo/DemoHelloApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoHelloApplication.class, args);
+    }
+
+}
