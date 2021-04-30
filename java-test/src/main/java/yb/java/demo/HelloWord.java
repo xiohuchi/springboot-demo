3c75f37b3e1f45582827351cb68c6458fdb1500c
杨滨 <bin.yang3@zhenai.com>
2021-04-30 12:16:15 +0800

    HelloWord

---
+++ java-test/src/main/java/yb/java/demo/HelloWord.java
@@ -0,0 +1,11 @@
+package yb.java.demo;
+
+/**
+ * @author 杨滨
+ * @date 2021年04月30日
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("HelloWord");
+    }
+}
